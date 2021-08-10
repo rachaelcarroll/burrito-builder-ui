0debe08715e0810aed0b923f15fda8c080043406
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-10 12:14:08 -0400

    Add POST

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,16 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-  .then(response => {
-    if(!response.ok) {
-      throw Error('Error fetching orders')
-    }
-    return response.json()
+export const getOrders = async () => {
+  const response = await fetch('http://localhost:3001/api/v1/orders')
+  const data = await response.json()
+  return data
+}
+
+export const postOrder = async (burrito) => {
+  const response = await fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(burrito)
   })
+
+  const data = await response.json()
+  return data
 }
