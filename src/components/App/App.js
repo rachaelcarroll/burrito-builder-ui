7c761db333b8d0fe2836bc7edfa96adaa6ac4c21
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-10 11:43:09 -0400

    Refactor App to use hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      orders: [],
-      error: ''
-    }
-  }
 
-  componentDidMount() {
+const App = () => {
+  const [ orders, setOrders ] = useState([]);
+  const [ error, setError ] = useState('');
+
+
+  useEffect(() => {
     getOrders()
-      .then(data => this.setState({ orders: data.orders }))
-      .catch(() => this.setState({ error: 'Error loading orders...please try again later.'} ));
-  }
+      .then(data => setOrders(data.orders))
+      .catch(err => setError(err))
+  }, [orders])
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
-
-        <Orders orders={this.state.orders}/>
-      </main>
-    );
-  }
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+      {error && <h2>{error}</h2>}
+        <OrderForm 
+          />
+      </header>
+        <Orders 
+        orders={orders}
+        />
+    </main>
+  );
 }
 
 export default App;
